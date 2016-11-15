e254193516e6614b9ed24d20767181a1059838fd
TheFump <alexandre.guth@etu.utc.fr>
2016-11-15 16:35:57 +0100

    24: Ajout composant playerstat

--- src/ihmTable/controller/TableController.java
+++ src/ihmTable/controller/TableController.java
@@ -11,7 +11,8 @@ import javafx.scene.layout.BorderPane;
 public class TableController {
 
 	private static double leftRightPanelWidth = 300;
-	private static double topBottomPanelHeight = 100;
+	private static double BottomPanelHeight = 200;
+	private static double TopPanelHeight = 100;
 
 	@FXML
 	private BorderPane tableView;
@@ -19,6 +20,7 @@ public class TableController {
 	public void initialize() throws IOException {
 		tableView.setRight(getCollapsiblePane("/ihmTable/resources/view/Chat.fxml", Position.right));
 		tableView.setLeft(getCollapsiblePane("/ihmTable/resources/view/Rules.fxml", Position.left));
+		//tableView.setBottom(getCollapsiblePane("/ihmTable/resources/view/PlayerStats.fxml", Position.bottom));
 	}
 
 	private AnchorPane getCollapsiblePane(String content, Position position) throws IOException {
@@ -30,8 +32,12 @@ public class TableController {
 		AnchorPane anchorPane = FXMLLoader.load(getClass().getResource(content));
 		if(position == Position.left || position == Position.right) {
 			panelController.setCollapsiblePanel(anchorPane, position, leftRightPanelWidth);
-		} else {
-			panelController.setCollapsiblePanel(anchorPane, position, topBottomPanelHeight);
+		} else if (position == Position.bottom) {
+			panelController.setCollapsiblePanel(anchorPane, position, BottomPanelHeight);
+		}
+		else
+		{
+			panelController.setCollapsiblePanel(anchorPane, position, BottomPanelHeight);
 		}
 		return panel;
 	}
