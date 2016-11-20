3e6eb878f7ef41c9e70d786e34228f34288c9f1a
jeanDupas <jean.dupas@etu.utc.fr>
2016-11-20 23:33:49 +0100

    25: Implémentation méthode login et enterMyPassword de InterfaceDataIHMLobby

--- src/data/IPData.java
+++ src/data/IPData.java
@@ -3,8 +3,75 @@ package data;
 public class IPData {
 	private String value;
 
+	/**
+	 * 
+	 */
+	public IPData() {
+		super();
+	}
+
 	public IPData(String value) {
 		this.value = value;
 	}
-	
+
+	/**
+	 * @return the value
+	 */
+	public String getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value
+	 *            the value to set
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		IPData other = (IPData) obj;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "IPData [value=" + value + "]";
+	}
+
 }
