cbcceb5db8cbd2ac826ef6885eb4fd54fd73c55d
TheFump <alexandre.guth@etu.utc.fr>
2016-11-22 14:18:28 +0100

    Retirer un main

--- src/ihmTable/IHMTable.java
+++ src/ihmTable/IHMTable.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 
 public class IHMTable extends Application {
 
-	public static void main(String[] args) {
+	public static void launch(String[] args) {
 		launch(args);
 	}
 
