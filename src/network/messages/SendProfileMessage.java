8c552117968c55e52d28f8f55a5ef8a6171e1831
Alexandre THOUVENIN <Alexandre THOUVENIN>
2016-11-22 20:47:16 +0100

    modifications suite au rebase

--- src/network/messages/SendProfileMessage.java
+++ src/network/messages/SendProfileMessage.java
@@ -2,7 +2,9 @@ package network.messages;
 
 import java.util.UUID;
 
+import data.ClientDataEngine;
 import data.Profile;
+import data.ServerDataEngine;
 
 public class SendProfileMessage implements IMessage{
 
@@ -15,16 +17,16 @@ public class SendProfileMessage implements IMessage{
 		this.receiver = receiver;
 		this.profile = profile;
 	}
-	
-	@Override
-	public void process() {
-		/**
-		 * TODO
-		 * Use interface of DATA-Local
-		 */
-		// displayProfile(profile);
-		
-		
-	}
 
+    @Override
+    public void process(ServerDataEngine dataEngine) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void process(ClientDataEngine dataEngine) {
+        // TODO Auto-generated method stub
+        
+    }
 }
