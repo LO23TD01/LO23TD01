d4cdbedace7174ceed0c0f9a48d9b966d5f64629
TheFump <alexandre.guth@etu.utc.fr>
2016-12-04 21:12:54 +0100

    Ajout des bindings (en partie) + ajout de 3 labels dans la partie #119

--- src/ihmTable/controller/PlayerStatsController.java
+++ src/ihmTable/controller/PlayerStatsController.java
@@ -2,6 +2,8 @@ package ihmTable.controller;
 
 import java.io.IOException;
 
+import data.GameTable;
+import data.PlayerData;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
@@ -61,7 +63,21 @@ public class PlayerStatsController {
     @FXML
     private AnchorPane PlayerStats_StatsPane;
 
+    @FXML
+    private Label PlayerStats_Label_Wins;
+
+    @FXML
+    private Label PlayerStats_Label_Lose;
+
+    @FXML
+    private Label PlayerStats_Label_Forfeit;
+
     private DiceController dice1, dice2, dice3;
+
+    public GameTable gameTableInstance;
+
+    private PlayerData currentPlayerData;
+
     // Création des images
     Image Jeton = new Image("/ihmTable/resources/png/JetonOK.png");
     Image JetonLoss = new Image("/ihmTable/resources/png/JetonLoss.png");
@@ -76,20 +92,77 @@ public class PlayerStatsController {
         PlayerStats_Jeton_Loss_Score_Label.setText("0");
         PlayerStats_Jeton_Loss_Score_Label.setTextFill(Color.RED);
 
-        //Bindings
-        //titlelableplayer
-        //scorelabel
-        //scorelabel
-        //scoreloss
 
-
-
-
+        Bindings();
 
         setPlayerDice();
     }
 
-    private void handleAsserts() {
+    private void Bindings()
+    {
+    	if(currentPlayerData != null)
+    	{
+    	//on crée un player data local pour pouvoir l'updater en fonction du joueur courant.
+        currentPlayerData = gameTableInstance.getGameState().getData(gameTableInstance.getGameState().getActualPlayer(), false);
+
+        //listeners
+        currentPlayerData.chipProperty().addListener((observable, oldValue, newValue) -> chipListener());
+        currentPlayerData.getPlayer().getPublicData(); //todo parties perdues
+        currentPlayerData.getPlayer().getPublicData().nbGameWonProperty().addListener((observable, oldValue, newValue) -> totalWinListener());
+        currentPlayerData.getPlayer().getPublicData().nbGameLostProperty().addListener((observable, oldValue, newValue) -> totalLossListener());
+        currentPlayerData.getPlayer().getPublicData().nbGameAbandonnedProperty().addListener((observable, oldValue, newValue) -> totalForfeitListener());
+
+        //bindings
+        currentPlayerData.getPlayer().getPublicData(); //todo name
+    	}
+    	else
+    	{
+    		//todo handle error
+    	}
+    }
+
+    private int totalForfeitListener() {
+    	if(currentPlayerData.getPlayer().getPublicData().getNbGameAbandonned() != 0)
+    	{
+    		PlayerStats_Label_Forfeit.setText(String.valueOf(currentPlayerData.getPlayer().getPublicData().getNbGameAbandonned()));
+    		return currentPlayerData.getPlayer().getPublicData().getNbGameAbandonned();
+    	}
+    	else
+    		return 0;
+	}
+
+
+    private int totalLossListener() {
+    	if(currentPlayerData.getPlayer().getPublicData().getNbGameWon() != 0)
+    	{
+    		PlayerStats_Label_Lose.setText(String.valueOf(currentPlayerData.getPlayer().getPublicData().getNbGameLost()));
+    		return currentPlayerData.getPlayer().getPublicData().getNbGameLost();
+    	}
+    	else
+    		return 0;
+	}
+
+	private int totalWinListener() {
+    	if(currentPlayerData.getPlayer().getPublicData().getNbGameWon() != 0)
+    	{
+    		PlayerStats_Label_Wins.setText(String.valueOf(currentPlayerData.getPlayer().getPublicData().getNbGameWon()));
+    		return currentPlayerData.getPlayer().getPublicData().getNbGameWon();
+    	}
+    	else
+    		return 0;
+	}
+
+	private int chipListener() {
+    	if(currentPlayerData.getChip() != 0)
+    	{
+    		PlayerStats_Jeton_Loss_Score_Label.setText(String.valueOf(currentPlayerData.getChip()));
+    		return currentPlayerData.getChip();
+    	}
+    	else
+    		return 0;
+	}
+
+	private void handleAsserts() {
         assert PLayerStats_VBoxLeft1 != null : "fx:id=\"PLayerStats_VBoxLeft1\" was not injected: check your FXML file 'PlayerStats.fxml'.";
         assert PlayerStats_TitlePane1 != null : "fx:id=\"PlayerStats_TitlePane1\" was not injected: check your FXML file 'PlayerStats.fxml'.";
         assert PlayerStats_TitleLabelPlayer != null : "fx:id=\"PlayerStats_TitleLabelPlayer\" was not injected: check your FXML file 'PlayerStats.fxml'.";
