144cd6becee3651cbd230930a83788e828800e0b
klajaeti <etienne.klaja@etu.utc.fr>
2016-12-06 20:12:52 +0100

    KLAJA : Corrigé des bugs, et implémenté des Trucs

--- src/data/client/InterImplDataTable.java
+++ src/data/client/InterImplDataTable.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import data.GameTable;
+import data.Parameters;
 import data.PlayerData;
+import data.Profile;
+import data.Rules;
 import data.User;
 import data.UserRole;
+import data.Variant;
 import data.Vote;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 
 public class InterImplDataTable implements InterfaceDataIHMTable{
 	private final ObjectProperty<GameTable> actualTable;
@@ -18,8 +23,15 @@ public class InterImplDataTable implements InterfaceDataIHMTable{
 	//constructeur  de test pour ihm table
 	public InterImplDataTable() {
 		super();
-		this.actualTable = null;
-		this.actualRole = null;
+
+		this.actualTable = new SimpleObjectProperty<GameTable>();
+		User u1 = new User(new Profile("lol","Jeanlaque","AHAH","Pro","Gamer",42));
+		User u2 = new User(new Profile("hackzorDu60","xXDeathKillerXx","AHAH","Kevin","Louzeur",10));
+		List<User> uList = new ArrayList<User>();
+		uList.add(u1);
+		uList.add(u2);
+		this.setActualTable(new GameTable("Table Test pour Ihm", u1, new Parameters(2,6,21,true,true,new Rules(Variant.CONSTRAINED_DISCHARGE,3)), uList, new ArrayList<User>()));
+		this.actualRole = new SimpleObjectProperty<UserRole>();
 	}
 
 	/**
@@ -29,23 +41,31 @@ public class InterImplDataTable implements InterfaceDataIHMTable{
 	public InterImplDataTable(ClientDataEngine dataEngine) {
 		super();
 		this.dataEngine = dataEngine;
-		this.actualTable = null;
-		this.actualRole = null;
+		this.actualTable = new SimpleObjectProperty<GameTable>();
+		this.actualRole = new SimpleObjectProperty<UserRole>();
+		if(this.dataEngine.getActualTable()==null)
+			;//throw exception
+		if(this.dataEngine.getActualRole()==null)
+			;//thwor new Escpetion
+		this.setActualTable(this.dataEngine.getActualTable());
+		this.setActualRole(this.dataEngine.getActualRole());
 	}
 
 	/**
 	 * @param actualTable
 	 * @param actualRole
 	 */
-	public InterImplDataTable(ObjectProperty<GameTable> actualTable, ObjectProperty<UserRole> actualRole) {
+	//Constructeur en copie
+	public InterImplDataTable(GameTable actualTable, UserRole actualRole) {
 		super();
-		this.actualTable = actualTable;
-		this.actualRole = actualRole;
+		this.actualTable = new SimpleObjectProperty<GameTable>(actualTable);
+		this.actualRole = new SimpleObjectProperty<UserRole>(actualRole);
 	}
 
 
 
-
+///IMPLEMENTATION INTERFACE ICI
+/////////////////////////////////////////////////////////////
 
 	@Override
 	public UserRole getUserRole() {
@@ -88,8 +108,10 @@ public class InterImplDataTable implements InterfaceDataIHMTable{
 
 	@Override
 	public void refuseReplay() {
-		this.dataEngine.getComClientInterface().refuseReplay(profileManager.get().getCurrentProfile().getUUID());
+		this.dataEngine.getComClientInterface().refuseReplay(this.dataEngine.getProfileManager().getCurrentProfile().getUUID());
 	}
+
+/////////////////////////////////////////////////////////////
 
 
 
@@ -118,8 +140,5 @@ public class InterImplDataTable implements InterfaceDataIHMTable{
 	public final void setActualRole(final UserRole actualRole) {
 		this.actualRoleProperty().set(actualRole);
 	}
-
-
-
 
 }
