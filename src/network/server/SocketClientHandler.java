1f4a560623e8909dd69464d6c5bdac99830bec90
CamilSa <camilsadiki@gmail.com>
2016-12-07 15:37:22 +0100

    Issue 130: Javadoc comments on network server and client

--- src/network/server/SocketClientHandler.java
+++ src/network/server/SocketClientHandler.java
@@ -9,23 +9,31 @@ import java.net.Socket;
 import network.messages.ConnectionMessage;
 import network.messages.IMessage;
 
+/**
+ * Handles the client's connection to the server
+ */
 public class SocketClientHandler implements Runnable{
-	
+
 	private Socket 				clientSocket;
 	private ObjectInputStream 	inputStream;
 	private ObjectOutputStream 	outputStream;
 	private ComServer server;
-	
+
 	/*
-	 * 
+	 *
 	 * Constructor
-	 * 
+	 *
 	 */
 
+	/**
+	 * SocketClientHandler's constructor's
+	 * @param clientSocket The socket associated with the connection
+	 * @param server Server to connect
+	 */
 	public SocketClientHandler(Socket clientSocket, ComServer server){
 		this.clientSocket = clientSocket;
 		this.server = server;
-		
+
 		try {
 			outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
 			outputStream.flush();
@@ -34,31 +42,34 @@ public class SocketClientHandler implements Runnable{
 			e.printStackTrace();
 		}
 	}
-	
+
 	/*
-	 * 
+	 *
 	 * Overridden methods
-	 * 
+	 *
 	 */
-	
+
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
 	@Override
 	public void run() {
 		while (true) {
             try {
-            	
+
                 IMessage o = (IMessage) inputStream.readObject();
-                
+
                 if(o != null){
                 	if(o.getClass() == ConnectionMessage.class){
                 		ConnectionMessage message = (ConnectionMessage) o;
                 		server.replaceWithUUID(clientSocket.getInetAddress().toString(), message.uuid);
                 	}
-                		
-                	
+
+
                 	o.process(server.getDataEngine());
                 }
-                	
-                
+
+
             } catch (EOFException e) {
                 try {
 					outputStream.close();
@@ -66,7 +77,7 @@ public class SocketClientHandler implements Runnable{
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
-                
+
                 Thread.currentThread().interrupt();
                 break;
             } catch (IOException e) {
@@ -76,10 +87,14 @@ public class SocketClientHandler implements Runnable{
             }
         }
 	}
-	
+
+	/**
+	 * Sends a message to the server
+	 * @param message Message to send
+	 */
 	public void sendMessage(IMessage message){
 		try {
-        	
+
             outputStream.writeObject(message);
             outputStream.flush();
         } catch (IOException e) {
