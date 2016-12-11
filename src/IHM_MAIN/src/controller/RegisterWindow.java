668e6ecfd26a830ecdb0c7ab8e6eb407e5a0e2bf
augustin72 <viot.augustin@gmail.com>
2016-12-11 20:40:53 +0100

    viotaugu : binding done. Just need to verify, I will have to commetn some line from  clientDataEngine to make it work because the pull is not compiling

--- src/IHM_MAIN/src/controller/RegisterWindow.java
+++ src/IHM_MAIN/src/controller/RegisterWindow.java
@@ -1,28 +1,36 @@
 package IHM_MAIN.src.controller;
 
+import data.client.ClientDataEngine;
+import data.client.InterfaceDataIHMLobby;
 import javafx.event.ActionEvent;
 import javafx.fxml.*;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
-
 public class RegisterWindow{
-		
-	@FXML
-	TextField pseudoField;
+	//FXML field instances from view :
+
 	@FXML
 	TextField loginField;
 	@FXML
-	TextField nomField;
-	@FXML
-	TextField prenomField;
-	@FXML
-	TextField ageField;
+	TextField pswdField;
 	
+	//FXML button instance from the view
 	@FXML
 	Button validerButton;
 	
+	//we need these tow instances and the corresponding setters so we can then use them to access the data/IHM-Lobby interface methods
+	data.client.ClientDataEngine clientData;
+    data.client.InterfaceDataIHMLobby interfaceData;
+    //setters for the ClientDataEngine and InterfaceDataIHMLobby instances
+    public void setClientData(ClientDataEngine client){
+		this.clientData = client;
+	}
+    
+	public void setInterfaceDataIHM(InterfaceDataIHMLobby interf){
+		this.interfaceData = interf;
+	}
 	
 	public RegisterWindow(){
 			
@@ -36,6 +44,7 @@ public class RegisterWindow{
 	@FXML
 	private void handleValiderButton() {
 		//send create profile request to server
+		this.interfaceData.createProfile(this.loginField.getText(), this.pswdField.getText());
 	}
 	
 	
