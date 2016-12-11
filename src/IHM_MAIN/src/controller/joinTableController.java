02feacadec2321ce85b668a825f6cc3d9b36fb8b
Tyrannas <someonefefe@gmail.com>
2016-12-11 22:52:16 +0100

    binding methods joinTable

--- src/IHM_MAIN/src/controller/joinTableController.java
+++ src/IHM_MAIN/src/controller/joinTableController.java
@@ -1,5 +1,8 @@
 package IHM_MAIN.src.controller;
 
+import data.GameTable;
+import data.client.ClientDataEngine;
+import data.client.InterfaceDataIHMLobby;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
@@ -11,15 +14,30 @@ public class joinTableController {
 	Button asSpec;
 	@FXML
 	Button cancel;
+	InterfaceDataIHMLobby interfaceData;
+	ClientDataEngine clientData;
+	GameTable joiningGame;
 	
+	public void setClientData(ClientDataEngine client){
+        this.clientData = client;
+    }
+    public void setInterfaceDataIHM(InterfaceDataIHMLobby interf){
+        this.interfaceData = interf;
+    }
+    public void setJoiningGame(GameTable game){
+        this.joiningGame = game;
+    }
+    @FXML
 	private void handleJoinAP(){
-		
+    	interfaceData.askJoinTable(joiningGame, true);
+    	//compléter avec ouverture table de jeu 
 	}
-	
+    @FXML
 	private void handleJoinAS(){
-		
+		interfaceData.askJoinTable(joiningGame, false);
+		//compléter avec ouveture table de jeu
 	}
-	
+    @FXML
 	private void handleCancel(){
 		Stage stage = (Stage) cancel.getScene().getWindow();
 	    stage.close();
