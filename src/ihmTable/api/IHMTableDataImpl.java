abaffb01fb5ca930c4bfd158db3e430d500d3fb1
Kapilraj Thangeswaran <tkapilraj@gmail.com>
2017-01-06 16:05:43 +0100

    Mise à jour interface data

--- src/ihmTable/api/IHMTableDataImpl.java
+++ src/ihmTable/api/IHMTableDataImpl.java
@@ -1,9 +1,35 @@
 package ihmTable.api;
 
+import java.util.Optional;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.StageStyle;
+
 public class IHMTableDataImpl implements IHMTableData {
+
+	private static final String ERROR_ALERT_HEADER = "Une erreur s'est produite";
 
 	@Override
 	public void closeTable() {
 		IHMTableLobbyImpl.getStage().close();
+	}
+
+	@Override
+	public void closeTable(String message) {
+		showErrorAlert(message);
+	}
+
+	private void showErrorAlert(String message) {
+		Alert alert = new Alert(AlertType.ERROR);
+		alert.setHeaderText(ERROR_ALERT_HEADER);
+		alert.setContentText(message);
+    	alert.initStyle(StageStyle.UNDECORATED);
+
+    	Optional<ButtonType> result = alert.showAndWait();
+    	if (result.get() == ButtonType.OK) {
+    		closeTable();
+    	}
 	}
 }
